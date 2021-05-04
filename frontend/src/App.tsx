5b44a95929a436988d0427eeffafb68616796c43
MichSide <michel-amoral@hotmail.com>
2021-05-04 14:16:28 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
+import DataTable from 'assets/components/DataTable';
+import Footer from 'assets/components/Footer';
+import NavBar from 'assets/components/NavBar';
 import React from 'react';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo!</h1>
+    <>
+    <NavBar />
+    <div className="container">
+      <h1 className="text">Olá Mundo!</h1>
+
+      <DataTable/>
     </div>
+    <Footer/>
+    
+    </>
   );
 }
 
